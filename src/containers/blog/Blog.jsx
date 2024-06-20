bfa104e2a318650bd09ea106cf82d1075a9201f8
NavCGaur <vertigo1112@gmail.com>
2024-06-20 07:55:45 +0530

    responsiveness of blog corrected

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -21,10 +21,10 @@ const Blog = () => (
         <Article imgUrl={blog01} date="Sep 26, 2021" text="GPT-4 and Open  AI is the future. Let us exlore how it is?" />
       </div>
       <div className="gpt4__blog-container_groupB">
-        <Article imgUrl={blog02} date="Sep 26, 2021" text="GPT-3544 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog03} date="Sep 26, 2021" text="GPT-46767 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog04} date="Sep 26, 2021" text="GPT-4567 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog05} date="Sep 26, 2021" text="GPT-46765 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog02} date="Sep 26, 2021" text="GPT-4 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog03} date="Sep 26, 2021" text="GPT-4 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog04} date="Sep 26, 2021" text="GPT-4 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog05} date="Sep 26, 2021" text="GPT-4 and Open  AI is the future. Let us exlore how it is?" />
       </div>
     </div>
   </div>
